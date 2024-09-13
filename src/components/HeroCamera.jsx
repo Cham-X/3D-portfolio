e4b7a24693e00357e0ad1cbf123a048daa2d72c4
Shamsudeen <zakariyyahshamsudeen@gmail.com>
2024-09-13 06:32:20 +0100

    first commit

---
+++ src/components/HeroCamera.jsx
@@ -0,0 +1,26 @@
+import { useFrame } from "@react-three/fiber"
+import { easing } from "maath";
+import { useRef } from "react"
+
+
+function HeroCamera({ children, isMobile }) {
+
+    const groupRef = useRef();
+
+    useFrame((state, delta) => {
+
+        easing.damp3(state.camera.position, [0, 0, 18], 0.25, delta);
+
+        if (isMobile) {
+            easing.dampE(groupRef.current.location, [state.pointer.y / 3, state.location.x / 5, 18], 0.25, delta);
+        }
+    })
+
+    return (
+        <group ref={groupRef}>
+            {children}
+        </group>
+    )
+}
+
+export default HeroCamera
